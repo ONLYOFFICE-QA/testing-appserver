527a78dd5acae971deac335f7cc444dc2994107f
Olga Nesterova <olganesterova034@gmail.com>
2021-11-11 01:21:34 -0800

    Test split

--- spec/main_page_links_spec.rb
+++ spec/main_page_links_spec.rb
@@ -15,35 +15,72 @@ describe 'Main page links' do
     expect(@main_page).to be_all_modules_present
   end
 
-  it 'All side bar modules open correctly' do
-    documents_page = @main_page.side_bar(:documents)
-    expect(documents_page).to be_a TestingAppServer::AppServerDocuments
-    people_page = documents_page.side_bar(:people)
-    expect(people_page).to be_a TestingAppServer::AppServerPeople
-    settings_page = people_page.side_bar(:settings)
-    expect(settings_page).to be_a TestingAppServer::AppServerSettings
-    projects_page = settings_page.side_bar(:projects)
-    expect(projects_page).to be_selected_module_picture_present(:projects)
-    calendar_page = projects_page.side_bar(:calendar)
-    expect(calendar_page).to be_selected_module_picture_present(:calendar)
-    mail_page = calendar_page.side_bar(:mail)
-    expect(mail_page).to be_selected_module_picture_present(:mail)
-    crm_page = mail_page.side_bar(:crm)
-    expect(crm_page).to be_selected_module_picture_present(:crm)
+  describe 'Side bar modules' do
+    it 'Documents module opens correctly from side bar' do
+      documents_page = @main_page.side_bar(:documents)
+      expect(documents_page).to be_a(TestingAppServer::DocumentsModule)
+    end
+
+    it 'People module opens correctly from side bar' do
+      people_page = @main_page.side_bar(:people)
+      expect(people_page).to be_a(TestingAppServer::PeopleModule)
+    end
+
+    it 'Settings module opens correctly from side bar' do
+      settings_page = @main_page.side_bar(:settings)
+      expect(settings_page).to be_a(TestingAppServer::SettingsModule)
+    end
+
+    it 'Projects place holder opens correctly from side bar' do
+      projects_page = @main_page.side_bar(:projects)
+      expect(projects_page).to be_selected_module_picture_present(:projects)
+    end
+
+    it 'Calendar place holder opens correctly from side bar' do
+      calendar_page = @main_page.side_bar(:calendar)
+      expect(calendar_page).to be_selected_module_picture_present(:calendar)
+    end
+
+    it 'Mail place holder opens correctly from side bar' do
+      mail_page = @main_page.side_bar(:mail)
+      expect(mail_page).to be_selected_module_picture_present(:mail)
+    end
+
+    it 'CRM place holder opens correctly from side bar' do
+      crm_page = @main_page.side_bar(:crm)
+      expect(crm_page).to be_selected_module_picture_present(:crm)
+    end
   end
 
-  it 'All main page modules open correctly' do
-    documents_page = @main_page.main_page(:documents)
-    expect(documents_page).to be_a TestingAppServer::AppServerDocuments
-    people_page = documents_page.home_top_logo.main_page(:people)
-    expect(people_page).to be_a TestingAppServer::AppServerPeople
-    projects_page = people_page.home_top_logo.main_page(:projects)
-    expect(projects_page).to be_selected_module_picture_present(:projects)
-    calendar_page = projects_page.home_top_logo.main_page(:calendar)
-    expect(calendar_page).to be_selected_module_picture_present(:calendar)
-    mail_page = calendar_page.home_top_logo.main_page(:mail)
-    expect(mail_page).to be_selected_module_picture_present(:mail)
-    crm_page = mail_page.home_top_logo.main_page(:crm)
-    expect(crm_page).to be_selected_module_picture_present(:crm)
+  describe 'Main page modules' do
+    it 'Documents module opens correctly from main page' do
+      documents_page = @main_page.main_page(:documents)
+      expect(documents_page).to be_a(TestingAppServer::DocumentsModule)
+    end
+
+    it 'People module opens correctly from main page' do
+      people_page = @main_page.main_page(:people)
+      expect(people_page).to be_a(TestingAppServer::PeopleModule)
+    end
+
+    it 'Projects place holder opens correctly from main page' do
+      projects_page = @main_page.main_page(:projects)
+      expect(projects_page).to be_selected_module_picture_present(:projects)
+    end
+
+    it 'Calendar place holder opens correctly from main page' do
+      calendar_page = @main_page.main_page(:calendar)
+      expect(calendar_page).to be_selected_module_picture_present(:calendar)
+    end
+
+    it 'Mail place holder opens correctly from main page' do
+      mail_page = @main_page.main_page(:mail)
+      expect(mail_page).to be_selected_module_picture_present(:mail)
+    end
+
+    it 'CRM place holder opens correctly from main page' do
+      crm_page = @main_page.main_page(:crm)
+      expect(crm_page).to be_selected_module_picture_present(:crm)
+    end
   end
 end
