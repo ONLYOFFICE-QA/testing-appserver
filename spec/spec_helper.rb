1109cd9f31a1a2cbe7997d9bcfd8118ee925da6b
Olga Nesterova <olganesterova034@gmail.com>
2022-04-20 00:54:56 -0700

    Replace shared_examples to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../lib/testing_appserver'
-require_relative '../shared_example/all_shared_examples'
+require_relative 'spec_helper/shared_examples'
 require 'onlyoffice_testrail_wrapper'
 require 'rspec'
