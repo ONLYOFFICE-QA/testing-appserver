baa1fa7a669ad5b2a6434cfdd196ce835871f4c6
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2022-05-19 01:36:27 -0700

    Groups actions redesign (#258)

--- lib/testing_appserver/modules/documents/modules/pop_up_windows/documents_move_to_trash.rb
+++ lib/testing_appserver/modules/documents/modules/pop_up_windows/documents_move_to_trash.rb
@@ -2,11 +2,11 @@
 
 module TestingAppServer
   # AppServer Move To Trash(Delete) window
-  # https://user-images.githubusercontent.com/40513035/154232964-f88cfdc7-0790-43b4-a0af-e7d46b5544cf.png
+  # https://user-images.githubusercontent.com/40513035/169240332-d318e16e-9bc8-46bb-93d8-8e24927f32e8.png
   module DocumentsMoveToTrash
     include PageObject
 
-    button(:move_to_trash, xpath: "//button[text()='Move to Trash']") # add_id
+    div(:move_to_trash, xpath: "//div[text()='Move to Trash']") # add_id
     div(:deleting_process_icon, xpath: "//div[contains(@class, 'layout-progress-bar')]")
     div(:success_delete_toast, xpath: "//div[contains(@class, 'Toastify__toast--success')]")
 
