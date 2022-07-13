b4694ccb9bd79507834c721c0df2c3836b779488
Kobiljon Komilov <38238032+Qobiljonn@users.noreply.github.com>
2022-07-13 16:48:18 +0500

    Reverting some deleted methods (#318)

--- lib/testing_appserver/data/helper_files/helper_files.rb
+++ lib/testing_appserver/data/helper_files/helper_files.rb
@@ -7,6 +7,10 @@ module TestingAppServer
       "#{Dir.pwd}/lib/testing_appserver/data/helper_files/"
     end
 
+    def self.path_to_tmp_file
+      "#{SampleFilesLocation.path_to_file}tmp/"
+    end
+
     def self.file_by_format
       {
         xlsx: 'my_spreadsheet.xlsx',
@@ -16,6 +20,16 @@ module TestingAppServer
         mp3: 'Track_9.mp3',
         jpg: 'my_picture.jpg'
       }
+    end
+
+    def self.upload_to_tmp_folder(file)
+      file_extension = file.split('.')[-1].to_sym
+      FileUtils.cp(SampleFilesLocation.path_to_file + SampleFilesLocation.file_by_format[file_extension],
+                   SampleFilesLocation.path_to_tmp_file + file)
+    end
+
+    def self.delete_from_tmp_folder(file)
+      FileUtils.rm(SampleFilesLocation.path_to_tmp_file + file)
     end
 
     def self.copy_file_to_temp(file)
