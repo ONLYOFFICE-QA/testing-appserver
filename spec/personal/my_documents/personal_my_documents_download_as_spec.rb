49f9754a56afe540e765fcc5972b923e96d0607e
Kobiljon Komilov <qobiljon3108@gmail.com>
2022-07-19 19:37:59 +0500

    Change temporary files generation to Tempfiles

--- spec/personal/my_documents/personal_my_documents_download_as_spec.rb
+++ spec/personal/my_documents/personal_my_documents_download_as_spec.rb
@@ -1,23 +1,28 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'tempfile'
 
 test_manager = TestingAppServer::PersonalTestManager.new(suite_name: File.basename(__FILE__))
 
 admin = TestingAppServer::PersonalUserData.new
 api_admin = TestingAppServer::ApiHelper.new(admin.portal, admin.mail, admin.pwd)
-new_document = "#{TestingAppServer::GeneralData.generate_random_name('New_Document')}.docx"
-new_spreadsheet = "#{TestingAppServer::GeneralData.generate_random_name('New_Spreadsheet')}.xlsx"
-new_presentation = "#{TestingAppServer::GeneralData.generate_random_name('New_Presentation')}.pptx"
+new_document = Tempfile.new(%w[New_Document .docx])
+new_spreadsheet = Tempfile.new(%w[New_Spreadsheet .xlsx])
+new_presentation = Tempfile.new(%w[New_Presentation .pptx])
 all_files = [new_document, new_spreadsheet, new_presentation]
 all_files.each do |file|
-  TestingAppServer::SampleFilesLocation.upload_to_tmp_folder(file)
-  api_admin.documents.upload_to_my_document(TestingAppServer::SampleFilesLocation.path_to_tmp_file + file)
+  TestingAppServer::SampleFilesLocation.copy_file_to_temp(file)
+  api_admin.documents.upload_to_my_document(file.path)
+end
+
+all_files_titles = all_files.map do |file|
+  File.basename(file)
 end
 
 describe 'Personal My Documents Download As' do
   after :all do
-    api_admin.documents.delete_files_by_title(all_files)
+    api_admin.documents.delete_files_by_title(all_files_titles)
     all_files.each { |file| TestingAppServer::SampleFilesLocation.delete_from_tmp_folder(file) }
   end
 
@@ -34,7 +39,7 @@ describe 'Personal My Documents Download As' do
   TestingAppServer::GeneralData.download_formats.each do |file_format, formats_to_convert|
     formats_to_convert.each do |format_to_convert|
       it "[Personal][My Documents] Download `#{file_format}` as `#{format_to_convert}` works" do
-        file_title = @documents_page.file_by_extension(all_files, file_format)
+        file_title = @documents_page.file_by_extension(all_files_titles, file_format)
         @documents_page.file_settings(file_title, :download_as, format: format_to_convert)
         expect(@documents_page).to be_file_downloaded(@documents_page.converted_file(file_title, file_format, format_to_convert))
       end
