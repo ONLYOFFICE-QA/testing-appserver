dbb1500a1de54fc3b763830d23c21f3611e1db9c
Kobiljon Komilov <qobiljon3108@gmail.com>
2022-07-21 19:02:19 +0500

    Fix spec for recent

--- spec/personal/recent/personal_recent_smoke_spec.rb
+++ spec/personal/recent/personal_recent_smoke_spec.rb
@@ -10,7 +10,7 @@ api_admin = TestingAppServer::ApiHelper.new(admin.portal, admin.mail, admin.pwd)
 new_document = TestingAppServer::GeneralData.generate_random_name('New_Document')
 new_spreadsheet = TestingAppServer::GeneralData.generate_random_name('New_Spreadsheet')
 new_presentation = TestingAppServer::GeneralData.generate_random_name('New_Presentation')
-all_files = ["#{new_document}.docx", "#{new_spreadsheet}.xlsx", "#{new_presentation}.pptx"]
+all_files = %W[#{new_document}.docx #{new_spreadsheet}.xlsx #{new_presentation}.pptx]
 
 # create recent files
 @test = TestingAppServer::PersonalTestInstance.new(admin)
@@ -36,14 +36,14 @@ describe 'Documents Recent' do
     @test.webdriver.quit
   end
 
-  it_behaves_like 'documents_recent_smoke', new_document, new_spreadsheet, new_presentation
+  it_behaves_like 'documents_recent_smoke', new_document, all_files
 
   it '[AppServer][Recent] Search field Filters are present' do
     expect(@recent).to be_docx_xlsx_pptx_filters_present
   end
 
   it '[AppServer][Recent] All group filters fore Recent present: Download, Download as, Copy' do
-    @recent.check_file_checkbox("#{new_document}.docx")
+    @recent.check_file_checkbox(new_document)
     expect(@recent).to be_download_copy_download_as_present
     expect(@recent.group_menu_share_file_element).not_to be_present
     expect(@recent.group_menu_move_to_element).not_to be_present
--- spec/spec_helper/shared_examples/documents_recent_smoke.rb
+++ spec/spec_helper/shared_examples/documents_recent_smoke.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-shared_examples_for 'documents_recent_smoke' do |new_document, new_spreadsheet, new_presentation|
+shared_examples_for 'documents_recent_smoke' do |new_document, all_files|
   it '[Recent] Actions menu button is disable' do
     expect(@recent).not_to be_actions_enabled
   end
 
   it '[Recent] Documents created and opened in My Documents exist in Recent' do
-    expect(@recent).to be_file_present(new_document)
-    expect(@recent).to be_file_present(new_spreadsheet)
-    expect(@recent).to be_file_present(new_presentation)
+    all_files.each do |file|
+      expect(@recent).to be_file_present(file)
+    end
   end
 
   it '[Recent] `Select all` Filters are present' do
