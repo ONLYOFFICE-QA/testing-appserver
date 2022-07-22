bccefe99a3659e12eea3b62cf79e1a4e4a64b64d
Kobiljon Komilov <38238032+Qobiljonn@users.noreply.github.com>
2022-07-22 16:58:28 +0500

    Add spec for search field (#337)
    * Add spec for search field
    
    * Add spec for search field
    
    * Add spec for search field
    
    * Add spec for search field
    
    Implement #293
    
    * Add spec for search field
    
    Implement #293
    
    * Add spec for search field
    
    Implement #293
    
    * Add spec for search field
    
    Implement #293
    
    * Add spec for search field
    
    Implement #293

---
+++ lib/testing_appserver/modules/documents/modules/documents_search_field.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module TestingAppServer
+  # AppServer Documents filter
+  # https://user-images.githubusercontent.com/40513035/174546418-10b55013-5abf-42f1-83cd-5e83588ac554.png
+  module DocumentsSearchField
+    include PageObject
+
+    text_field(:search_field, xpath: "//input[contains(@class,'search-input-block')]")
+
+    def search_file(document_name)
+      search_field_element.send_keys(document_name)
+    end
+
+    def wait_until_not_found
+      @instance.webdriver.wait_until { not_found_visible? }
+    end
+
+    def not_found_visible?
+      not_found_xpath = "//div[contains(@class, 'section-scroll scroll-body')]//span[text() = 'Nothing found']"
+      @instance.webdriver.element_visible?(not_found_xpath)
+    end
+  end
+end
---
+++ spec/personal/my_documents/personal_my_document_search_field_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'tempfile'
+
+test_manager = TestingAppServer::PersonalTestManager.new(suite_name: File.basename(__FILE__))
+
+admin = TestingAppServer::PersonalUserData.new
+api_admin = TestingAppServer::ApiHelper.new(admin.portal, admin.mail, admin.pwd)
+
+first_document = Tempfile.new(%w[New_Document .docx])
+second_document = Tempfile.new(%w[New_Document .docx])
+temp_docs = [first_document, second_document]
+temp_docs_titles = [File.basename(first_document, '.docx'), File.basename(second_document, '.docx')]
+
+temp_docs.each do |file|
+  TestingAppServer::SampleFilesLocation.copy_file_to_temp(file)
+  api_admin.documents.upload_to_my_document(file.path)
+end
+
+describe 'Documents search field' do
+  before do
+    @test = TestingAppServer::PersonalTestInstance.new(admin)
+    @documents_page = TestingAppServer::PersonalSite.new(@test).personal_login(admin.mail, admin.pwd)
+  end
+
+  after :all do
+    api_admin.documents.delete_files_by_title(temp_docs_titles)
+  end
+
+  after do |example|
+    test_manager.add_result(example, @test)
+    @test.webdriver.quit
+  end
+
+  it 'Search field works' do
+    pending('https://bugzilla.onlyoffice.com/show_bug.cgi?id=58239')
+    @documents_page.search_file(temp_docs_titles[0])
+    expect(@documents_page).to be_file_present(temp_docs_titles[0])
+    expect(@documents_page).not_to be_file_present(temp_docs_titles[1])
+  end
+
+  it "'Nothing Found' works" do
+    random_document = TestingAppServer::GeneralData.generate_random_name('My_document')
+    @documents_page.search_file(random_document)
+    @documents_page.wait_until_not_found
+    expect(@documents_page).to be_not_found_visible
+  end
+end
