ea787777883dcc6e6916713a1f2b536c1ed454ab
Kobiljon Komilov <qobiljon3108@gmail.com>
2022-07-26 14:06:02 +0500

    Add smoke spec for Trash folder
    Implement #144

---
+++ lib/testing_appserver/modules/documents/modules/pop_up_windows/document_empty_trash.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module TestingAppServer
+  # Appserver empty Trash pop up
+  #https://user-images.githubusercontent.com/38238032/180782386-b20b4d63-516a-4dc4-8fe9-b903feebd516.jpg
+  module DocumentsEmptyTrash
+    include PageObject
+    svg_xpath = "/*[name() = 'svg' and contains(@data-src, 'images/clear.trash.react.svg')]"
+
+    svg(:header_trash_icon, xpath: "//div[contains(@class, 'icon-button_svg not-selectable')]#{svg_xpath}")
+    svg(:sidebar_trash_icon, xpath: "//div[contains(@class, 'catalog-icon__badge')]#{svg_xpath}")
+    svg(:close_icon, xpath: "//div[contains(@class, 'modal-dialog-content')]//*[name() = 'svg' and contains(@class, 'modal-dialog-button_close')]")
+
+    button(:delete_button, xpath: "//div[contains(@class, 'modal-dialog-modal-footer')]/button[1]")
+    button(:cancel_button, xpath: "//div[contains(@class, 'modal-dialog-modal-footer')]/button[2]")
+
+    def empty_trash_header_button_clicked
+      header_trash_icon_element.click
+      delete_button
+      @instance.webdriver.wait_until { !deleting_process_icon_element.present? }
+      @instance.webdriver.wait_until { success_delete_toast_element.present? }
+      @instance.webdriver.wait_until { !success_delete_toast_element.present? }
+    end
+
+    def empty_trash_sidebar_button_clicked
+      sidebar_trash_icon_element.click
+      delete_button
+      @instance.webdriver.wait_until { !deleting_process_icon_element.present? }
+      @instance.webdriver.wait_until { success_delete_toast_element.present? }
+      @instance.webdriver.wait_until { !success_delete_toast_element.present? }
+    end
+
+    def empty_trash_cancel_button_clicked
+      header_trash_icon_element.click
+      cancel_button
+    end
+
+    def empty_trash_close_icon_clicked
+      header_trash_icon_element.click
+      close_icon_element.click
+    end
+  end
+end
---
+++ spec/personal/trash/personal_trash_smoke_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'tempfile'
+
+test_manager = TestingAppServer::PersonalTestManager.new(suite_name: File.basename(__FILE__))
+
+admin = TestingAppServer::PersonalUserData.new
+api_admin = TestingAppServer::ApiHelper.new(admin.portal, admin.mail, admin.pwd)
+
+describe 'Trash folder for Personal' do
+
+  before do
+    @document = Tempfile.new(%w[My_Document .docx])
+    TestingAppServer::SampleFilesLocation.copy_file_to_temp(@document)
+    api_admin.documents.upload_to_my_document(@document.path)
+    @test = TestingAppServer::PersonalTestInstance.new(admin)
+    @my_documents = TestingAppServer::PersonalSite.new(@test).personal_login(admin.mail, admin.pwd)
+    @my_documents.file_settings(File.basename(@document), :delete)
+    @trash = @my_documents.documents_navigation(:trash)
+    @document = File.basename(@document)
+  end
+
+  after do |example|
+    test_manager.add_result(example, @test)
+    @test.webdriver.quit
+  end
+
+  it '`Restore` option works' do
+    @trash.check_file_checkbox(@document)
+    @trash.restore_from_trash
+    expect(@trash).not_to be_file_present(@document)
+    @documents = @my_documents.documents_navigation(:my_documents)
+    expect(@documents).to be_file_present(@document)
+  end
+
+  it '`Empty trash` header button works' do
+    @trash.empty_trash_header_button_clicked
+    expect(@trash).to be_trash_empty
+  end
+
+  it '`Empty trash` sidebar button works' do
+    @trash.empty_trash_sidebar_button_clicked
+    expect(@trash).to be_trash_empty
+  end
+
+  it '`Cancel` button closes pop up window' do
+    @trash.empty_trash_cancel_button_clicked
+    expect(@trash).to be_file_present(@document)
+  end
+
+  it '`Close` icon closes pop up window' do
+    @trash.empty_trash_close_icon_clicked
+    expect(@trash).to be_file_present(@document)
+  end
+end
